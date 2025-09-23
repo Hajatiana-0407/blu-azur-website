eddd829a0ba7f0c88f7794e759af43a49b1f1184
Hajatiana-0407 <hajatianarasolondraibe0407@gmail.com>
2025-09-23 21:13:51 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import './index.css'
+const App = () => {
+  return (
+    <div className='text-red-500 text-center p-10 text-8xl'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id eaque nam accusantium vel recusandae assumenda.</div>
+  )
+}
+
+export default App
