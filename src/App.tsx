2614f1bee16190621041a1e3a532008b2b73672b
Hajatiana-0407 <hajatianarasolondraibe0407@gmail.com>
2025-09-25 12:06:04 +0300

    feat: add dicover component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Footer from './components/layout/Footer'
 import Header from './components/layout/Header'
 import './index.css'
 import DestinationHighlights from './pages/DestinationHighlights'
+import Discover from './pages/Discover'
 import Feature from './pages/Feature'
 import Hero from './pages/Hero'
 import Location from './pages/Location'
@@ -10,9 +11,8 @@ import Location from './pages/Location'
 const App = () => {
   return (
     <div className='h-screen'>
-
-      {/* Entete */}
       <Header />
+      
       <section id='about'>
         <Hero />
         <Feature />
@@ -20,8 +20,10 @@ const App = () => {
       </section>
 
       <section id='location'>
-        <Location  />
+        <Location />
+        <Discover />
       </section>
+
       <Footer />
     </div>
   )
---
+++ src/pages/Discover.tsx
@@ -0,0 +1,40 @@
+import ImageDiscoverBg from '../assets/images/discover.png'
+import ImageDiscoverGroup from "../assets/images/dicoverGroupImage.png"
+
+const Discover = () => {
+    return (
+        <section
+            className="relative bg-no-repeat bg-cover bg-top py-20 text-white"
+            style={{
+                backgroundImage: `linear-gradient(to left, rgba(var(--color-theme-rgb), 1),rgba(var(--color-theme-rgb), 1) , rgba(0,0,0,0.2)), url(${ImageDiscoverBg})`,
+            }}
+        >
+            <div className='__container'>
+                <div className='grid grid-cols-1  md:grid-cols-2 gap-10 items-center justity-around'>
+
+                    {/* Content left  */}
+                    <div className="mb-10 max-md:flex max-md:justify-center">
+                        <img src={ImageDiscoverGroup} alt="Discover group" className="rounded-xl shadow-lg w-[394px]" />
+                    </div>
+
+                    {/* Content right */}
+                    <div className="space-y-10 text-right max-md:text-center">
+                        <div>
+                            <h2 className="text-[2.5rem] leading-tight">
+                                Un point de départ idéal pour {" "}
+                                <span className="block font-bold">découvrir la Côte d’Azur ! </span>
+                            </h2>
+                        </div>
+                        <div>
+                            <p className="text-lg text-center md:text-justify">
+                                Entre mer et montagne, Saint-Raphaël est une destination idéale pour les amateurs de plein air et de découvertes. Randonnées dans le Massif de l’Estérel, sports nautiques sur les eaux cristallines de la Méditerranée, balades en bateau vers les îles de Lérins ou encore golf et cyclisme sur des parcours panoramiques, chaque journée offre son lot d’aventures. Entre marchés provençaux, villages perchés et animations locales, l’art de vivre azuréen se dévoile à chaque instant, faisant de Saint-Raphaël un lieu vibrant et inoubliable.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Discover
